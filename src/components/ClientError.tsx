08ec5c814c7b3f841ffa5d8c9ddfbe6444a6af8b
cosmomojo <sonicismael2@gmail.com>
2024-02-27 20:10:56 +0000

    Fixing overflow issues on webkit. Remove typo

--- src/components/ClientError.tsx
+++ src/components/ClientError.tsx
@@ -5,7 +5,7 @@ const ClientError = () => {
 
     <>
       <div>
-        <Section id={''} title={'Client Request Error'} />=
+        <Section id={''} title={'Client Request Error'} />
       </div>
       <p>Unable to reach the server, have you lost internet connection? You could be blocking CORS requests, try fixing that and refreshing.</p>
     </>
