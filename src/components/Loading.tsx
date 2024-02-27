14f4fb49d8298a2722ba5882d649d7fb73da9335
Ismael Hamilton El-Aquil <ismael.b.hamilton@gmail.com>
2024-02-27 16:38:47 +0000

    Added loading spinner! Adapted from CompuIves on Codepen! Thanks so much, the animation is rad.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,20 @@
+import './Loading.scss';
 
 const Loading = () => {
   return (
-    <div>Loading...</div>
+    <div className='loadingContainer'>
+      <div className="cube">
+        <div className="sides">
+          <div className="top"></div>
+          <div className="right"></div>
+          <div className="bottom"></div>
+          <div className="left"></div>
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+    </div>
+
   );
 }
 
